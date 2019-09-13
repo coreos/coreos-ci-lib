a102895790973a0d4d7c70fd948da141844085c7
Colin Walters <walters@verbum.org>
2019-09-13 12:58:49 +0000

    pod: Support `runAsUser: 0`
    I think this is better than calling it `privileged` as that
    has a specific meaning in Docker/Kubernetes.
    
    See my argument at https://lwn.net/Articles/796885/

--- vars/coreos.groovy
+++ vars/coreos.groovy
@@ -1,17 +1,22 @@
 // Run the passed body in a pod respecting some parameters.
 // Available parameters:
 //   image: string
-//   privileged: bool
 //   kvm: bool
+//   runAsUser: int
+//   privileged: bool (deprecated, equivalent to `runAsUser: 0`)
 def pod(params, body) {
     def podJSON = libraryResource 'com/github/coreos/pod.json'
     def podObj = readJSON text: podJSON
     podObj['spec']['containers'][1]['image'] = params['image']
 
     if (params['privileged']) {
+        // Backwards compat, see https://github.com/projectatomic/rpm-ostree/pull/1899/commits/9c1709c363e94760f0e9461719b92a7a4aca6c63#r323256575
+        params['runAsUser'] = 0
+    }
+    if (params['runAsUser']) {
         // XXX: tmp hack to get anyuid SCC; need to ask to get jenkins SA added
         podObj['spec']['serviceAccountName'] = "papr"
-        podObj['spec']['containers'][1]['securityContext'] = [runAsUser: 0]
+        podObj['spec']['containers'][1]['securityContext'] = [runAsUser: params['runAsUser']]
     }
 
     if (params['kvm']) {

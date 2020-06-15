135b70c998c964c4174a6ddbcb0cabb6bf6cd54e
Jonathan Lebon <jonathan@jlebon.com>
2020-06-15 11:23:41 -0400

    shwrap: only set HOME if unset or /
    E.g. we don't want to override it if `$HOME` is correctly set.

--- vars/shwrap.groovy
+++ vars/shwrap.groovy
@@ -1,6 +1,10 @@
 def call(cmds) {
-    // default is HOME=/ which normally we don't have access to
-    withEnv(["HOME=${env.WORKSPACE}"]) {
+    envs = []
+    // override default of HOME=/ which normally we don't have access to
+    if (env.HOME == null || env.HOME == "/") {
+        envs += "HOME=${env.WORKSPACE}"
+    }
+    withEnv(envs) {
         sh """
             set -xeuo pipefail
             ${cmds}

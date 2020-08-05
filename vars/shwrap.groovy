45762ba5aded40365a4777cfa462e76b737f60ad
Colin Walters <walters@verbum.org>
2020-08-05 14:41:37 +0000

    shwrap: Fix umask if it's not set
    We never want a 0 umask; it can e.g. cause world writable directories
    which triggers failures in e.g. ostree's CI tests.

--- vars/shwrap.groovy
+++ vars/shwrap.groovy
@@ -1,8 +1,12 @@
 def call(cmds) {
-    // default is HOME=/ which normally we don't have access to
+    // default is HOME=/ which normally we don't have access to.
+    // Also if umask is somehow unset, fix it.
     withEnv(["HOME=${env.WORKSPACE}"]) {
         sh """
             set -xeuo pipefail
+            if [ `umask` = 0000 ]; then
+              umask 0022
+            fi
             ${cmds}
         """
     }

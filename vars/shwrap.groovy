53252cced7eac4aa362faa99177cf6fed26b888b
Jonathan Lebon <jonathan@jlebon.com>
2020-08-11 11:51:03 -0400

    Revert "shwrap: Fix umask if it's not set"
    This reverts commit 45762ba5aded40365a4777cfa462e76b737f60ad.
    
    This is fixed now in the cluster:
    https://pagure.io/centos-infra/issue/48

--- vars/shwrap.groovy
+++ vars/shwrap.groovy
@@ -1,12 +1,8 @@
 def call(cmds) {
-    // default is HOME=/ which normally we don't have access to.
-    // Also if umask is somehow unset, fix it.
+    // default is HOME=/ which normally we don't have access to
     withEnv(["HOME=${env.WORKSPACE}"]) {
         sh """
             set -xeuo pipefail
-            if [ `umask` = 0000 ]; then
-              umask 0022
-            fi
             ${cmds}
         """
     }

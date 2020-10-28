5c65e85d80af283e783976575f272cf76e1fae27
Renata Ravanelli <rravanel@redhat.com>
2020-10-28 17:57:24 -0300

    utils: Add cosaBuild and unitTest
    Make the cosa build process with unit tests available
    in utils
    
    Signed-off-by: Renata Ravanelli <rravanel@redhat.com>

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,7 +1,48 @@
+def cosaBuild(params = [:]) {
+// Available parameters:
+//     gitInstall:         boolean -- install git package
+//     gitClone:           boolean -- clone cosa
+//     skipUnitTest:       boolean -- do not run unit tests
+    stage("Build Cosa") {
+        if (params['gitInstall']) {
+            shwrap(""" dnf install -y git """)
+        }
+
+        if (params['gitClone']) {
+            shwrap("""
+                git clone https://github.com/coreos/coreos-assembler.git
+                cd coreos-assembler && git submodule update --init
+                ./build.sh
+            """)
+        } else {
+            shwrap("""
+                git submodule update --init  && ./build.sh
+            """)
+       }
+       shwrap("""
+           rpm -qa | sort > rpmdb.txt
+       """)
+       archiveArtifacts artifacts: 'rpmdb.txt'
+    }
+
+    if (!params['skipUnitTest']) {
+        unitTest()
+    }
+}
+
 def getCosaDir(params = [:]) {
     if (params['cosaDir']) {
         return params['cosaDir']
     } else {
         return "/srv/fcos"
+    }
+}
+
+def unitTest() {
+    stage("Unit Test") {
+        shwrap("""
+            make check
+            make unittest
+        """)
     }
 }

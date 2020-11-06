8ae8209185304269689178cdf4efba3fb6d564a6
Renata Ravanelli <rravanel@redhat.com>
2020-11-06 16:51:30 -0300

    utils: Add cosaCmd
    Add cosaCmd in order to easily run cosa commands
    
    Signed-off-by: Renata Ravanelli <rravanel@redhat.com>

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,3 +1,18 @@
+def cosaCmd(params = [:]) {
+// Available parameters:
+//     cosaDir:        string  -- cosa working directory
+//     args:           string  -- arguments to pass to `cosa`
+//     user:           string  -- user to run the cosa commands 
+    def cosaDir = getCosaDir(params)
+    def args = params.get('args', "");
+
+    if(!params['user']) {
+        shwrap("cd ${cosaDir} && cosa ${args}")
+    } else {
+        shwrap("cd ${cosaDir} && sudo -u ${params['user']} cosa ${args}")
+    }
+}
+
 def getCosaDir(params = [:]) {
     if (params['cosaDir']) {
         return params['cosaDir']

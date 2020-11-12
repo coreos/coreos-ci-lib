a6ae64a52ea17dbd14d2221b4902325751db5690
Renata Ravanelli <rravanel@redhat.com>
2020-11-12 10:35:10 -0300

    utils/cosaCmd: Add stage parameter
    Signed-off-by: Renata Ravanelli <rravanel@redhat.com>

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,10 +1,15 @@
 def cosaCmd(params = [:]) {
 // Available parameters:
-//     cosaDir:        string  -- cosa working directory
 //     args:           string  -- arguments to pass to `cosa`
+//     cosaDir:        string  -- cosa working directory
+//     stage:          string  -- CI stage name
 //     user:           string  -- user to run the cosa commands 
     def cosaDir = getCosaDir(params)
     def args = params.get('args', "");
+
+    if (params['stage']) {
+        stage(params['stage'])
+    }
 
     if(!params['user']) {
         shwrap("cd ${cosaDir} && cosa ${args}")

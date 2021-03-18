ffddc40ae68874ab978b0dfd7baf0bb41a2c78e1
Jonathan Lebon <jonathan@jlebon.com>
2021-03-18 15:43:31 -0400

    utils: add pathExists
    Upstreamed from
    https://github.com/coreos/fedora-coreos-pipeline/blob/5cf784ba931627c1c08dbb9f09080c57542480c9/utils.groovy#L63.

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -20,3 +20,8 @@ def getCosaDir(params = [:]) {
         return "/srv/fcos"
     }
 }
+
+// This is like fileExists, but actually works inside the Kubernetes container.
+def pathExists(path) {
+    return shwrapRc("test -e ${path}") == 0
+}

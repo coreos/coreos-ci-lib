1b0e1ba3bc0c149d25e9c04d2c6bf7556d7e023d
Colin Walters <walters@verbum.org>
2021-04-07 14:00:10 +0000

    buildPod: Support overriding image
    So it's easier to test alternative images; motivated by
    https://github.com/coreos/fedora-coreos-config/pull/932

--- vars/buildPod.groovy
+++ vars/buildPod.groovy
@@ -4,7 +4,7 @@
 //   buildroot: bool
 def call(params = [:], Closure body) {
     def stream = params.get('stream', 'testing-devel');
-    params['image'] = "quay.io/coreos-assembler/fcos-buildroot:${stream}".toString()
+    params['image'] = params.get('image', "quay.io/coreos-assembler/fcos-buildroot:${stream}".toString());
 
     // we don't like zombies
     params['cmd'] = ["/usr/bin/dumb-init", "/usr/bin/sleep", "infinity"]

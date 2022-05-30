069359788ae378c3c030c4bb8063629dd2a50526
Renata Ravanelli <rravanel@redhat.com>
2022-05-30 10:37:51 -0300

    Add withPodmanRemote
    - Add wrapper to allow podman remote commands using the
    environment variables CONTAINER_SSHKEY and CONTAINER_HOST
    in Jenkins execution
    
    Signed-off-by: Renata Ravanelli <rravanel@redhat.com>

---
+++ vars/withPodmanRemote.groovy
@@ -0,0 +1,25 @@
+// Run podman remote context using the
+// environment variables CONTAINER_SSHKEY and CONTAINER_HOST
+// Available parameters:
+//    remoteHost:    string -- Credential ID containing the remote host
+//    remoteUid:     string -- Credential ID containing the remote user UID
+//    sshKey:        string -- Credential ID containing the private SSH key
+def call(params = [:], Closure body) {
+    withCredentials([
+        string(credentialsId: params['remoteHost'],
+               variable: 'REMOTEHOST'),
+        string(credentialsId: params['remoteUid'],
+               variable: 'REMOTEUID'),
+        sshUserPrivateKey(credentialsId: params['sshKey'],
+                          usernameVariable: 'REMOTEUSER',
+                          keyFileVariable: 'CONTAINER_SSHKEY')
+    ]) {
+        withEnv(["CONTAINER_HOST=ssh://${REMOTEUSER}@${REMOTEHOST}/run/user/${REMOTEUID}/podman/podman.sock"]) {
+            shwrap("""
+            # workaround bug: https://github.com/jenkinsci/configuration-as-code-plugin/issues/1646
+            sed -i s/^----BEGIN/-----BEGIN/ \$CONTAINER_SSHKEY
+            """)
+            body()
+        }
+    }
+}

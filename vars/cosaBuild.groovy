coreos/coreos-ci-lib

commit 3719f6934320a63329933a6a3a588e02c76b78f0

tree: drop FCOS wording from various places
Signed-off-by: Renata Ravanelli <rravanel@redhat.com>

vars/cosaBuild.groovy
@@ -1,4 +1,4 @@
-// Build FCOS, possibly with modifications.
+// Build CoreOS, possibly with modifications.
 // Available parameters:
 //    cosaDir:        string   -- Cosa working directory
 //    extraArgs:      string   -- Extra arguments to pass to `cosa build`
@@ -12,7 +12,7 @@
 //    skipInit:       boolean  -- Assume `cosa init` has already been run
 //    skipKola:       boolean  -- Do not automatically run kola on resulting build
 def call(params = [:]) {
-    stage("Build FCOS") {
+    stage("Build") {
         def cosaDir = utils.getCosaDir(params)
         def extraFetchArgs = params.get('extraFetchArgs', "");
         def extraArgs = params.get('extraArgs', "");
@@ -54,7 +54,7 @@ def call(params = [:]) {
     }
 
     if (!params['skipKola']) {
-        fcosKola()
+        kola()
     }
 }
 

vars/buildPod.groovy
@@ -1,4 +1,4 @@
-// buildPod is a thin wrapper around pod() which fills in the FCOS buildroot images.
+// buildPod is a thin wrapper around pod() which fills in the CoreOS buildroot images.
 // Additional available parameters:
 //   image: string
 //   buildroot: bool

vars/kola.groovy
@@ -30,8 +30,8 @@ def call(params = [:]) {
     // Prevents multiple runs overwriting same filename in archiveArtifacts
     def token = shwrapCapture("uuidgen | cut -f1 -d-")
 
-    // Create a unique output directory for this run of fcosKola
-    def outputDir = shwrapCapture("mktemp -d ${cosaDir}/tmp/fcosKola-XXXXX")
+    // Create a unique output directory for this run of kola
+    def outputDir = shwrapCapture("mktemp -d ${cosaDir}/tmp/kola-XXXXX")
 
     // This is a bit obscure; what we're doing here is building a map of "name"
     // to "closure" which `parallel` will run in parallel. That way, we can

vars/utils.groovy
@@ -17,7 +17,7 @@ def getCosaDir(params = [:]) {
     if (params['cosaDir']) {
         return params['cosaDir']
     } else {
-        return "/srv/fcos"
+        return "/srv/coreos"
     }
 }
 

commit aa4058473312e3718ca4d90b5948b26f50b09a73

vars/utils: add compat symlink from /srv/fcos to /srv/coreos
Commit 3719f69 ("tree: drop FCOS wording from various places") broke
multiple upstream CI jobs which still reference the old location. We
need to temporarily support both locations as we migrate them.

Add a compat symlink.

vars/cosaBuild.groovy
@@ -19,6 +19,14 @@ def call(params = [:]) {
 
         shwrap("mkdir -p ${cosaDir}")
 
+        // if the cosa dir is the default, also add a symlink from /srv/fcos
+        // for backwards compatibility
+        shwrap("""
+            if [ ${cosaDir} = /srv/coreos ]; then
+                ln -s coreos /srv/fcos
+            fi
+        """)
+
         if (!params['skipInit']) {
             def branchArg = ""
             if (params['gitBranch']) {
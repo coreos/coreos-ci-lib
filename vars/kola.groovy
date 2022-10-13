d046e2bdc1905bf9f4db2466e5bfc4064d9eda49
Dusty Mabe <dusty@dustymabe.com>
2022-10-12 23:32:29 -0400

    {kola,kolaTestIso}: initial stab at multi-arch support
    The rule here is mostly to run things through either `cosa` commands
    or run generic bash commands through `cosa shell --`. This also is
    made easier if we're running the `cosa` command within `shwrap()`
    from the cosa dir already, so let's use `dir(cosaDir)` to try to
    enable that simplification.

--- vars/kola.groovy
+++ vars/kola.groovy
@@ -1,6 +1,7 @@
 // Run kola tests on the latest build in the cosa dir
 // Available parameters:
 //    addExtTests:       []string -- list of test paths to run
+//    arch:              string   -- the target architecture
 //    cosaDir:           string   -- cosa working directory
 //    parallel:          integer  -- number of tests to run in parallel (default: # CPUs)
 //    skipBasicScenarios boolean  -- skip basic qemu scenarios
@@ -16,22 +17,20 @@ def call(params = [:]) {
     // this is shared between `kola run` and `kola run-upgrade`
     def platformArgs = params.get('platformArgs', "");
     def buildID = params.get('build', "latest");
-    def arch = params.get('arch', "");
+    def arch = params.get('arch', "x86_64");
     def marker = params.get('marker', "kola");
     def rerun = "--rerun"
     if (params.get('disableRerun', false)) {
         rerun = ""
     }
-    if (arch != "") {
-        arch = "--arch=${arch}"
-    }
+    def archArg = "--arch=${arch}"
 
     // Define a unique token to be added to the file name uploads
     // Prevents multiple runs overwriting same filename in archiveArtifacts
     def token = shwrapCapture("uuidgen | cut -f1 -d-")
 
     // Create a unique output directory for this run of kola
-    def outputDir = shwrapCapture("mktemp -d ${cosaDir}/tmp/kola-XXXXX")
+    def outputDir = shwrapCapture("cosa shell -- mktemp -d ${cosaDir}/tmp/kola-XXXXX")
 
     // list of identifiers for each run for log collection
     def ids = []
@@ -55,9 +54,16 @@ def call(params = [:]) {
         {
             // The workspace name created by Jenkins is messy and dynamic, but
             // kola uses it to derive the test name. Let's fix it by using a
-            // symlinked dir.
+            // symlinked dir (x86_64) or copied dir (multi-arch).
             def name = shwrapCapture("basename \$(git config --get remote.origin.url) .git")
-            shwrap("mkdir -p /var/tmp/kola && ln -s ${env.WORKSPACE} /var/tmp/kola/${name}")
+            if (arch == 'x86_64') {
+                shwrap("mkdir -p /var/tmp/kola && ln -s ${env.WORKSPACE} /var/tmp/kola/${name}")
+            } else {
+                shwrap("""
+                cosa shell -- mkdir -p /var/tmp/kola
+                cosa remote-session sync ${env.WORKSPACE}/ :/var/tmp/kola/${name}/
+                """)
+            }
             args += "--exttest /var/tmp/kola/${name}"
         }
         def parallel = params.get('parallel', "auto");
@@ -72,41 +78,43 @@ def call(params = [:]) {
         if (!params['skipBasicScenarios']) {
             id = marker == "" ? "kola-basic" : "kola-basic-${marker}"
             ids += id
-            shwrap("cd ${cosaDir} && cosa kola run ${rerun} --output-dir=${outputDir}/${id} --basic-qemu-scenarios")
+            shwrap("cosa kola run ${rerun} --output-dir=${outputDir}/${id} --basic-qemu-scenarios")
         }
         // normal run (without reprovision tests because those require a lot of memory)
         id = marker == "" ? "kola" : "kola-${marker}"
         ids += id
-        shwrap("cd ${cosaDir} && cosa kola run ${rerun} --output-dir=${outputDir}/${id} --build=${buildID} ${arch} ${platformArgs} --tag '!reprovision' --parallel ${parallel} ${args} ${extraArgs}")
+        shwrap("cosa kola run ${rerun} --output-dir=${outputDir}/${id} --build=${buildID} ${archArg} ${platformArgs} --tag '!reprovision' --parallel ${parallel} ${args} ${extraArgs}")
 
         // re-provision tests (not run with --parallel argument to kola)
         id = marker == "" ? "kola-reprovision" : "kola-reprovision-${marker}"
         ids += id
-        shwrap("cd ${cosaDir} && cosa kola run ${rerun} --output-dir=${outputDir}/${id} --build=${buildID} ${arch} ${platformArgs} --tag reprovision ${args} ${extraArgs}")
+        shwrap("cosa kola run ${rerun} --output-dir=${outputDir}/${id} --build=${buildID} ${archArg} ${platformArgs} --tag reprovision ${args} ${extraArgs}")
     }
 
     if (!params["skipUpgrade"]) {
         kolaRuns['run_upgrades'] = {
             def id = marker == "" ? "kola-upgrade" : "kola-upgrade-${marker}"
             ids += id
-            shwrap("cd ${cosaDir} && cosa kola ${rerun} --output-dir=${outputDir}/${id} --upgrades --build=${buildID} ${arch} ${platformArgs}")
+            shwrap("cosa kola ${rerun} --output-dir=${outputDir}/${id} --upgrades --build=${buildID} ${archArg} ${platformArgs}")
         }
     }
 
     stage('Kola') {
-        try {
-            if (kolaRuns.size() == 1) {
-                kolaRuns.each { k, v -> v() }
-            } else {
-                parallel(kolaRuns)
-            }
-        } finally {
-            for (id in ids) {
-                // sanity check kola actually ran and dumped its output
-                shwrap("test -d ${outputDir}/${id}")
-                // collect the output
-                shwrap("tar -c -C ${outputDir} ${id} | xz -c9 > ${env.WORKSPACE}/${id}-${token}.tar.xz")
-                archiveArtifacts allowEmptyArchive: true, artifacts: "${id}-${token}.tar.xz"
+        dir(cosaDir) {
+            try {
+                if (kolaRuns.size() == 1) {
+                    kolaRuns.each { k, v -> v() }
+                } else {
+                    parallel(kolaRuns)
+                }
+            } finally {
+                for (id in ids) {
+                    // sanity check kola actually ran and dumped its output
+                    shwrap("cosa shell -- test -d ${outputDir}/${id}")
+                    // collect the output
+                    shwrap("cosa shell -- tar -c --xz ${outputDir}/${id} > ${env.WORKSPACE}/${id}-${token}.tar.xz")
+                    archiveArtifacts allowEmptyArchive: true, artifacts: "${id}-${token}.tar.xz"
+                }
             }
         }
     }

172960bc9d1b28aa37bf342e847dbaa603feeabf
Jonathan Lebon <jonathan@jlebon.com>
2023-02-16 16:38:59 -0500

    Add `umask` workaround to other shwrap helpers
    Didn't hit anything caused by this, but we should be consistent across
    all the `shwrap` helpers on principle.

--- vars/shwrapCapture.groovy
+++ vars/shwrapCapture.groovy
@@ -1,6 +1,10 @@
 def call(cmds) {
+    // If umask is somehow unset, fix it.
     return sh(returnStdout: true, script: """
         set -euo pipefail
+        if [ `umask` = 0000 ]; then
+          umask 0022
+        fi
         ${cmds}
     """).trim()
 }

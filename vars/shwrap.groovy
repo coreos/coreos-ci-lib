7e9255e57f83d6c51acbab49957f526777398de9
Jonathan Lebon <jonathan@jlebon.com>
2023-02-16 16:35:19 -0500

    Set `HOME` at the pod level instead of in shwrap helpers
    That way we're sure it's always set.

--- vars/shwrap.groovy
+++ vars/shwrap.groovy
@@ -1,13 +1,10 @@
 def call(cmds) {
-    // default is HOME=/ which normally we don't have access to.
-    // Also if umask is somehow unset, fix it.
-    withEnv(["HOME=${env.WORKSPACE}"]) {
-        sh """
-            set -xeuo pipefail
-            if [ `umask` = 0000 ]; then
-              umask 0022
-            fi
-            ${cmds}
-        """
-    }
+    // If umask is somehow unset, fix it.
+    sh """
+        set -xeuo pipefail
+        if [ `umask` = 0000 ]; then
+          umask 0022
+        fi
+        ${cmds}
+    """
 }

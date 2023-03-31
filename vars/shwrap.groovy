6a46f8888eb84e54aa0e4954badf3cff1ac3655f
Jonathan Lebon <jonathan@jlebon.com>
2023-03-31 16:56:11 -0400

    Revert "Set `HOME` at the pod level instead of in shwrap helpers"
    This reverts commit 3577892a9cac46964a5f44be8881a9ef7741df8c.
    
    Setting the `HOME` var to the workspace from the pod definition doesn't
    work because it introduces a chicken-and-egg problem: the workspace
    isn't yet allocated since the pod isn't running yet.
    
    This isn't a pure revert since I wanted to keep 33d4910 ("Add `umask`
    workaround to other shwrap helpers"), which came after.

--- vars/shwrap.groovy
+++ vars/shwrap.groovy
@@ -1,10 +1,13 @@
 def call(cmds) {
-    // If umask is somehow unset, fix it.
-    sh """
-        set -xeuo pipefail
-        if [ `umask` = 0000 ]; then
-          umask 0022
-        fi
-        ${cmds}
-    """
+    // default is HOME=/ which normally we don't have access to.
+    // Also if umask is somehow unset, fix it.
+    withEnv(["HOME=${env.WORKSPACE}"]) {
+        sh """
+            set -xeuo pipefail
+            if [ `umask` = 0000 ]; then
+              umask 0022
+            fi
+            ${cmds}
+        """
+    }
 }

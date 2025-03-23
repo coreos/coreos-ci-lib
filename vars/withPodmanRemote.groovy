74be4a6807fd17b72108163ce602732499cbbc02
Dusty Mabe <dusty@dustymabe.com>
2025-03-23 13:51:37 -0400

    withPodmanRemote: drop the REMOTEUID in CONTAINER_HOST
    I don't think this is needed anymore with [1]. See [2] [3].
    
    [1] https://github.com/containers/podman/pull/23847
    [2] https://github.com/containers/podman/issues/17452
    [3] https://github.com/containers/podman/issues/23831

--- vars/withPodmanRemote.groovy
+++ vars/withPodmanRemote.groovy
@@ -2,19 +2,16 @@
 // environment variables CONTAINER_SSHKEY and CONTAINER_HOST
 // Available parameters:
 //    remoteHost:    string -- Credential ID containing the remote host
-//    remoteUid:     string -- Credential ID containing the remote user UID
 //    sshKey:        string -- Credential ID containing the private SSH key
 def call(params = [:], Closure body) {
     withCredentials([
         string(credentialsId: params['remoteHost'],
                variable: 'REMOTEHOST'),
-        string(credentialsId: params['remoteUid'],
-               variable: 'REMOTEUID'),
         sshUserPrivateKey(credentialsId: params['sshKey'],
                           usernameVariable: 'REMOTEUSER',
                           keyFileVariable: 'CONTAINER_SSHKEY')
     ]) {
-        withEnv(["CONTAINER_HOST=ssh://${REMOTEUSER}@${REMOTEHOST}:22/run/user/${REMOTEUID}/podman/podman.sock"]) {
+        withEnv(["CONTAINER_HOST=ssh://${REMOTEUSER}@${REMOTEHOST}"]) {
             shwrap("""
             # workaround bug: https://github.com/jenkinsci/configuration-as-code-plugin/issues/1646
             sed -i s/^----BEGIN/-----BEGIN/ \$CONTAINER_SSHKEY
